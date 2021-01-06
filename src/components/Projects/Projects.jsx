98bdbdc11dae8517cf7e2c820c9402ba3f5bae80
Ridwan Ajibola <rajibola1997@gmail.com>
2021-01-06 22:54:16 +0100

    feat: Completed project section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -24,96 +24,29 @@ const Projects = () => {
 
   return (
     <section id="projects">
-      <Container>
-        <div className="project-wrapper">
-          <Title title="Projects" />
-          {projects.map((project) => {
-            const { title, info, info2, url, repo, img, id } = project;
+      {projects.map((project) => {
+        const { title, info, info2, url, repo, img, id } = project;
 
-            return (
-              <Row key={id}>
-                <Col lg={4} sm={12}>
-                  <Fade
-                    left={isDesktop}
-                    bottom={isMobile}
-                    duration={1000}
-                    delay={500}
-                    distance="30px"
-                  >
-                    <div className="project-wrapper__text">
-                      <h3 className="project-wrapper__text-title">{title || 'Project Title'}</h3>
-                      <div>
-                        <p>
-                          {info ||
-                            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
-                        </p>
-                        <p className="mb-4">{info2 || ''}</p>
-                      </div>
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="cta-btn cta-btn--hero"
-                        href={url || '#!'}
-                      >
-                        See Live
-                      </a>
+        return (
+          <div className="flex-container">
+            <div className="flex-container__text">
+              <h2 className="title">{title}</h2>
+              <div className="line" />
+              <h3 className="info">{info}</h3>
+              <h3 className="info">{info2}</h3>
 
-                      {repo && (
-                        <a
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="cta-btn text-color-main"
-                          href={repo}
-                        >
-                          Source Code
-                        </a>
-                      )}
-                    </div>
-                  </Fade>
-                </Col>
-                <Col lg={8} sm={12}>
-                  <Fade
-                    right={isDesktop}
-                    bottom={isMobile}
-                    duration={1000}
-                    delay={1000}
-                    distance="30px"
-                  >
-                    <div className="project-wrapper__image">
-                      <a
-                        href={url || '#!'}
-                        target="_blank"
-                        aria-label="Project Link"
-                        rel="noopener noreferrer"
-                      >
-                        <Tilt
-                          options={{
-                            reverse: false,
-                            max: 8,
-                            perspective: 1000,
-                            scale: 1,
-                            speed: 300,
-                            transition: true,
-                            axis: null,
-                            reset: true,
-                            easing: 'cubic-bezier(.03,.98,.52,.99)',
-                          }}
-                        >
-                          <div data-tilt className="thumbnail rounded">
-                            <ProjectImg alt={title} filename={img} />
-                          </div>
-                        </Tilt>
-                      </a>
-                    </div>
-                  </Fade>
-                </Col>
-              </Row>
-            );
-          })}
-        </div>
-      </Container>
+              <div className="button-container">
+                <div className="button button__1">More on behance</div>
+                <div className="button button__2">online soon</div>
+              </div>
+            </div>
+
+            <div className="flex-container__image">
+              <ProjectImg classNam="project-image-base" alt={title} filename={img} height="400px" />
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 };
-
-export default Projects;
